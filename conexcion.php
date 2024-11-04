02bee06ba3fc885f0bf07262954eb8e5e57f4161
Gabotx4 <eseweyxd38@gmail.com>
2024-11-03 21:26:05 -0600

    se agrega la conexion

---
+++ conexcion.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "laravel";
+$username = "root";
+$password = "1218";
+$dbname = "login";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Ejemplo de consulta
+$sql = "SELECT * FROM tu_tabla";
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+echo json_encode($data);
+
+$conn->close();
+?>
